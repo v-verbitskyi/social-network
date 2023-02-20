4f971c8a978145b51cda8280abf0a2c7d4591cc7
Ivan Verbitskyi <ivan.verbitskyj@gmail.com>
2023-02-20 23:49:51 +0200

    light and dark theme created

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import { HomePage, LoginPage, Navbar, ProfilePage } from "scenes";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { themeSettings } from "theme";
 
 function App() {
+	const mode = useSelector((state) => state.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<LoginPage />} />
-					<Route path="/home" element={<HomePage />} />
-					<Route path="/profile/:userId" element={<ProfilePage />} />
-				</Routes>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route path="/" element={<LoginPage />} />
+						<Route path="/home" element={<HomePage />} />
+						<Route path="/profile/:userId" element={<ProfilePage />} />
+					</Routes>
+				</ThemeProvider>
 			</BrowserRouter>
 		</div>
 	);
---
+++ client/src/components/FlexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "Amui/material";
+import { styled } from "@mui/system";
+
+const FlexBetween = styled(Box)({
+	display: "flex",
+	justifyContent: "space-between",
+	alignItems: "center",
+});
+
+export default FlexBetween;
